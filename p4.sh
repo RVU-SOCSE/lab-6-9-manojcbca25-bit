e07daa16009e13eece854d0a3a6f3324f726c052
manojcbca25-bit <manojcbca25@rvu.edu.in>
2026-04-07 13:18:53 +0530

    Add files via upload

---
+++ p4.sh
@@ -0,0 +1,41 @@
+!/bin/bash
+
+echo "Lap 9: 1/0 Redirection Demo "
+
+#Output redirection (overwrite)
+
+echo "Hello, this is OS Lab" filel.txt
+
+echo "Content written to filel.txt"
+
+#Append output
+
+echo "Appending new line" >> filel.txt
+
+echo "Content appended to filel.txt"
+
+#Input redirection
+
+echo ""
+
+echo "Reading from filel.txt using input redirection:"
+
+cat file1.txt
+
+#Error redirection
+
+echo ""
+
+echo "Trying to read non-existing file:"
+
+cat file_not_exist.txt 2> error.txt
+
+echo "Error message stored in error.txt"
+
+# Display error file
+
+echo ""
+
+echo "Error file content:"
+
+cat error.txt
